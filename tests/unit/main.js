b8ace37f253e805659e166f7fc835aeba06ae930
zaphod1984 <zaphod84@gmx.de>
2013-06-03 13:44:49 +0200

    rudimentary tests added

--- tests/unit/main.js
+++ tests/unit/main.js
@@ -1,14 +1,40 @@
 var expect = require('chai').expect;
+var sinon = require('sinon');
 
 var cachify = require('../../');
 
 describe('main', function() {
 
     function FooClass() {}
-    var cached = cachify(new FooClass());
+
+    FooClass.prototype.one = sinon.stub().returns('one');
+    FooClass.prototype.two = sinon.stub().returns('two');
+
+    var fooObject = new FooClass();
+
+    var fooObjectOneStub = fooObject.one;
+    var fooObjectTwoStub = fooObject.two;
+
+    var cached = cachify(fooObject, {'one':[]});
+
+    cached.one();
+    cached.one();
+    cached.one();
+
+    cached.two();
+    cached.two();
+    cached.two();
 
     it('should return the same object', function() {
         expect(cached).to.be.an.instanceOf(FooClass);
+    });
+
+    it('should be called only once', function() {
+        expect(fooObjectOneStub.args.length).to.equal(1);
+    });
+
+    it('should be called thrice', function() {
+        expect(fooObjectTwoStub.args.length).to.equal(3);
     });
 
 });

8f4daa0b4d0066e01d12c2f62e342325f8617125
zaphod1984 <zaphod84@gmx.de>
2013-06-03 13:30:44 +0200

    added example file

---
+++ examples/one.js
@@ -0,0 +1,36 @@
+var cachify = require('../');
+
+function FooClass(b) {
+    this._a = 'foo';
+    this._b = b;
+
+    this.three = function() {
+        console.log( 'three invoked' );
+        return 'three';
+    }
+}
+
+//two should not be cached!
+FooClass.prototype.two = function() {
+    console.log('two actually invoked');
+    return 'two';
+};
+
+FooClass.prototype.one = function(a, b) {
+    console.log( 'one actually invoked' );
+    return '|' + b + '|' + this._a + '|' + this._b + '|' + this.two();
+};
+
+var fooObject = new FooClass('b');
+
+var fooObjectCached = cachify(fooObject, {'one':[1], 'three':[]});
+
+console.log( fooObjectCached.one(1,2) );
+console.log( fooObjectCached.one(2,2) );
+console.log( fooObjectCached.one(1,2) );
+
+console.log( fooObjectCached.two() );
+console.log( fooObjectCached.two() );
+
+console.log( fooObjectCached.three() );
+console.log( fooObjectCached.three() );

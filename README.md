bed2b3b468df1b93be301b9faf4b111e8e1aba6a
zaphod1984 <zaphod84@gmx.de>
2013-06-03 13:27:39 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 node-transparentCache
 =====================
+
+Invocation:
+```javascript
+var cachify = require('transparentCache');
+
+function FooClass(b) {
+    this._a = 'foo';
+    this._b = b;
+
+    this.three = function() {
+        console.log( 'three invoked' );
+        return 'three';
+    }
+}
+
+//two should not be cached!
+FooClass.prototype.two = function() {
+    console.log('two actually invoked');
+    return 'two';
+};
+
+FooClass.prototype.one = function(a, b) {
+    console.log( 'one actually invoked' );
+    return '|' + b + '|' + this._a + '|' + this._b + '|' + this.two();
+};
+
+var fooObject = new FooClass('b');
+
+var fooObjectCached = cachify(fooObject, {'one':[1], 'three':[]});
+
+console.log( fooObjectCached.one(1,2) );
+console.log( fooObjectCached.one(2,2) );
+console.log( fooObjectCached.one(1,2) );
+
+console.log( fooObjectCached.two() );
+console.log( fooObjectCached.two() );
+
+console.log( fooObjectCached.three() );
+console.log( fooObjectCached.three() );
+```
+
+yields the following output:
+```
+one actually invoked
+|2|foo|b|
+|2|foo|b|
+|2|foo|b|
+two actually invoked
+two
+two actually invoked
+two
+three invoked
+three
+three
+```
---
+++ index.js
@@ -0,0 +1,68 @@
+//TODO: accept a strategy that implements the way the cache values are stored
+//TODO: e.g. plain Object, LRU-Cache, ringBuffer...
+
+function cachify(client, config) {
+    config = config || {};
+
+    var cache = {};
+
+    Object.keys(config).forEach(function(augmentFunctionName) {
+        var _previous = client[augmentFunctionName];
+
+        var me = function() {
+            var collectedRelevantArguments = cachify._collectRelevantArguments(me._cacheData, [].slice.apply(arguments));
+            collectedRelevantArguments.unshift(augmentFunctionName);
+
+            var cacheName = cachify._createCacheName(collectedRelevantArguments);
+            var cacheContent = cachify._lookupCache(cache, cacheName);
+
+            if(cacheContent !== null) {
+                return cacheContent;
+            } else {
+                var returnValue = _previous.apply(client, arguments);
+
+                cachify._addToCache(cache, cacheName, returnValue);
+
+                return returnValue;
+            }
+        };
+
+        me._cacheData = config[augmentFunctionName];
+
+        client[augmentFunctionName] = me;
+    });
+
+    return client;
+}
+
+cachify._createCacheName = function(listOfArguments) {
+    return listOfArguments.join('_');
+};
+
+cachify._collectRelevantArguments = function(cacheData, allArguments) {
+    var relevantArguments = [];
+    var i = 0;
+    allArguments.forEach(function(argument) {
+        if(cacheData.indexOf(i) >= 0) {
+            relevantArguments.push(argument);
+        }
+
+        i++;
+    });
+
+    return relevantArguments;
+};
+
+cachify._lookupCache = function(cache, name) {
+    if(cache[name]) {
+        return cache[name];
+    }
+
+    return null;
+};
+
+cachify._addToCache = function(cache, name, value) {
+    cache[name] = value;
+};
+
+module.exports = cachify;
